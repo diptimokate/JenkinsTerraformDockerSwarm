be159061073aa1d623dac6ae4509b8d3f369d437
diptimokate <mokatedipti@gmail.com>
2026-08-15 19:02:32 +0530

    Jenkins Terraform Docker Swarm project files

---
+++ vars.tf
@@ -0,0 +1,24 @@
+variable "AWS_REGION" {
+    default = "ap-southeast-1"
+}
+variable "amazon_linux_id" {
+    default = "ami-05a67986ec8a2dcf8"
+}
+variable "akey" {
+    default = ""
+}
+variable "skey" {
+    default = ""
+}
+variable "instance_type" {
+    default = "t3.micro"
+}
+variable "pkey" {
+    default = "singapore-linux-keypair"
+}
+variable "az" {
+    default = "ap-southeast-1a"
+}
+variable "root_pass" {
+    default = "111"
+}
